539fdb834da71773666c64f3aa36af8013c015be
Paul Tarjan <spam@paulisageek.com>
2009-11-19 03:02:15 +0000

    don't need to blow up the .pyc if i don't have to

--- example/makezip.sh
+++ example/makezip.sh
@@ -6,5 +6,4 @@
 # decompresses .zip on its own and puts it in a funky
 # directory
 
-find hadoop_record -name '*.pyc' -exec rm {} \;
 zip hadoop_record.mod hadoop_record/*.py hadoop_record/ply/*.py
